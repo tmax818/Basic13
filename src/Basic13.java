0f19f1d34ed3ece0bfbdb1496917b05ed7c037f6
tmax818 <tylermaxwell661@gmail.com>
2022-09-25 20:44:36 -0700

    clean main

--- src/Basic13.java
+++ src/Basic13.java
@@ -12,9 +12,10 @@ public class Basic13 {
         // TODO: Given an array, find and print its largest element.
     }
 
-//    public int[] CreateArrayOfOdds1to255(){
-//        // TODO: Create an array with all the odd integers between 1 and 255 (inclusive).
-//    }
+    public int[] CreateArrayOfOdds1to255(){
+        // TODO: Create an array with all the odd integers between 1 and 255 (inclusive).
+        return null;
+    }
 
     public void PrintCountGreaterThanY(int[] intArray, int y){
         // TODO: Given an array and a value Y, count and print the number of array values greater than Y.
@@ -26,13 +27,36 @@ public class Basic13 {
 
     public Object[] ReplaceWithDojo(Object[] objArray){
         // TODO: Replace any negative array values with 'Dojo'.
+        return null;
     }
 
-// TODO: Print all odd integers from 1 to 255.
-// TODO: Iterate through a given array, printing each value.
-// TODO: Analyze an array’s values and print the average
-// TODO: Square each value in a given array, returning that same array with changed values
-// TODO: Return the given array, after setting any negative values to zero.
-// TODO: Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+    public void printAllOdd1To255(){
+        // TODO: Print all odd integers from 1 to 255.
+    }
+
+    public void PrintEachArrayValue(int[] intArray){
+        // TODO: Iterate through a given array, printing each value.
+
+    }
+
+    public void PrintArrayAverage(int[] intArray){
+        // TODO: Analyze an array’s values and print the average
+    }
+
+    public int[] SquareArrayValues(int[] intArray){
+        // TODO: Square each value in a given array, returning that same array with changed values
+        return null;
+    }
+
+    public int[] SetNegativeToZero(int[] intArray){
+        // TODO: Return the given array, after setting any negative values to zero.
+        return null;
+    }
+
+    public int[] ShiftForward(int[] intArray){
+        // TODO: Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+        return null;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Basic13 b13 = new Basic13();
-        Object[] theArray = {1,2,3,-4,6,7};
-        Object[] result = b13.ReplaceWithDojo(theArray);
-        System.out.println(Arrays.toString(result));
+        // TODO test algos here
     }
 }
